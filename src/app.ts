cce282864574d913a814db27ed56bcedef2e5dc2
Grady Barrett <Grady.Barrett@Gmail.com>
2020-06-14 20:16:15 -0700

    Introduce types to hello world

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
+import { Request, Response } from 'express'
+
 const express = require('express')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
